acb750185d4874e7958afc1ad024642956fdbd8f
daikijager <130050422+daikijager@users.noreply.github.com>
2023-11-17 16:11:55 +0700

    update

--- database/migrations/2023_11_17_000000_create_tasks_table.php
+++ database/migrations/2023_11_17_000000_create_tasks_table.php
@@ -1,32 +1,27 @@
 <?php
-
+ 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
-class CreateUsersTable extends Migration
+ 
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('note_name');
-            $table->timestamps();
+            $table->string('name');
         });
     }
-
+ 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('tasks');
+        Schema::drop('tasks');
     }
-}
+};
